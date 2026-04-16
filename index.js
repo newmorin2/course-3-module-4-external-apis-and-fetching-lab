833009f5796bf753a06edf5b63578751ef852b68
newmorin2 <newton.mwangi@student.moringaschool.com>
2026-04-16 15:12:06 +0300

    created the fetch weather alerts function

--- index.js
+++ index.js
@@ -2,3 +2,22 @@
 const weatherApi = "https://api.weather.gov/alerts/active?area="
 
 // Your code here!
+async function fetchWeatherAlerts(state) {
+  try {
+    const response = await fetch(`https://api.weather.gov/alerts/active?area=${state}`);
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch weather alerts");
+    }
+
+    const data = await response.json();
+
+    console.log(data); // for testing
+    displayAlerts(data);
+    clearError();
+
+  } catch (error) {
+    console.log(error.message);
+    showError(error.message);
+  }
+}
